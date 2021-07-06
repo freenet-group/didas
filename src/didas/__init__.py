2609360ae21422d3030a4ea0f4c286acfa9c0a48
Georg Wendorf <georgwendorf@gmail.com>
2021-07-06 15:43:11 +0200

    Update __init__.py

--- src/didas/__init__.py
+++ src/didas/__init__.py
@@ -1,1 +1,11 @@
+from collections import defaultdict
 
+def dic(o):
+    r = defaultdict(set)
+    for d in dir(o):
+        a = getattr(o, d)
+        t = type(a)
+        r[t].add(d)
+    return dict(r)
+
+__version__ = "0.1"
