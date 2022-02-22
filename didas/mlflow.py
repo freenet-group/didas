703820211a240ede0715937370bf53f94edbb37c
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-02-22 17:32:11 +0100

    Update mlflow.py

--- didas/mlflow.py
+++ didas/mlflow.py
@@ -1,4 +1,5 @@
 import os
+
 import google.auth.transport.requests
 import google.oauth2.id_token
 
@@ -8,8 +9,8 @@ def set_google_tracking_token(tracking_uri=None, tracking_token=None):
         os.environ["MLFLOW_TRACKING_URI"] = tracking_uri
     if tracking_token is None:
         os.environ["MLFLOW_TRACKING_TOKEN"] = google.oauth2.id_token.fetch_id_token(
-            request = google.auth.transport.requests.Request(),
-            audience = os.environ["MLFLOW_TRACKING_URI"],
+            request=google.auth.transport.requests.Request(),
+            audience=os.environ["MLFLOW_TRACKING_URI"],
         )
     else:
         os.environ["MLFLOW_TRACKING_TOKEN"] = tracking_token
