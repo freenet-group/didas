c26d185646e239a1d29dd338f6476802a87364b3
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-04-21 18:47:33 +0200

    Update __init__.py

--- didas/__init__.py
+++ didas/__init__.py
@@ -3,7 +3,9 @@ from collections import defaultdict
 try:
     from ._version import __version__
 except ImportError:
-    __version__ = "UNKNOWN"
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
 
 
 def dic(o):
