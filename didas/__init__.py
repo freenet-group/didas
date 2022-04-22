e3d9b800bc9d5977e3efe839d39c2fbdf0222ddb
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-04-22 11:04:53 +0200

    Update __init__.py

--- didas/__init__.py
+++ didas/__init__.py
@@ -6,7 +6,7 @@ except ImportError:
     try:
         from setuptools_scm import get_version
 
-        __version__ = get_version()
+        __version__ = get_version(root='..', relative_to=__file__)
     except (ImportError, LookupError) as e:
         __version__ = e
 
