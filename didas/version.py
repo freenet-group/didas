15cd6850c861dab64f69d8bccf9c284f16986826
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-04-22 12:40:56 +0200

    Delete version.py

--- didas/version.py
+++
@@ -1,9 +0,0 @@
-try:
-    from ._version import __version__
-except ImportError:
-    try:
-        from setuptools_scm import get_version
-
-        __version__ = get_version(root='..', relative_to=__file__)
-    except (ImportError, LookupError) as e:
-        __version__ = e
